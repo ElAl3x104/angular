92f39c599322453b5c5e6129c1f91a111ce1f126
Bonura_Alex <162216566+ElAl3x104@users.noreply.github.com>
2025-04-07 07:24:34 +0000

    quarto commit, Modificato

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { Article } from '../models/article.models';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,17 @@ import { ArticleComponent } from './article/article.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  articles:Article[];
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1)
+    ];
+  }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    let t = title.value;
-    let v = link.value;
-    console.log(`Adding article title: ` + t + ` and link: ` + v);
-    console.log(`Adding article title: ` + title.value + ` and link: ` + link.value);
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article('Angular 2', 'http://angular.io', 3));
     return false;
   }
+  
 }
